42e3c98cd810bb21ecd467e555452c3b1e3f8082
Divyash Chhetri <divyashchhetri@gmail.com>
2022-02-13 21:49:02 +0600

    Added Problem #4

---
+++ Problem_4/solution.py
@@ -0,0 +1,21 @@
+# Day 4
+# Problem 4
+# Date 13 Feb 2021
+# Time 9:20 PM
+
+# input -> [3, 4, -1, 1]
+# output -> 2
+
+# input -> [1, 2, 0]
+# output -> 3
+
+arr = input()
+
+unique = {*arr}
+print(unique)
+index = 1
+while True:
+    if index not in unique:
+        print(index)
+        break
+    index += 1
