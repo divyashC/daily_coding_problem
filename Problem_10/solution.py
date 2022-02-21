51b148901c78ae34b33bb978bdd01e5b28a71b54
Divyash Chhetri <divyashchhetri@gmail.com>
2022-02-21 18:56:06 +0600

    Added Problem #10

--- Problem_10/solution.py
+++ Problem_10/solution.py
@@ -1,4 +1,20 @@
 # Day 10
 # Problem 10
-# Date 19 Feb 2021
-# Time 10:40 PM
+# Date 21 Feb 2021
+# Time 6:50 PM
+
+import time
+
+
+def hello():
+    print("Hello")
+
+
+def scheduler(f, n):
+    time.sleep(n/1000)
+    f()
+
+
+scheduler(hello, 0)
+scheduler(hello, 1000)
+scheduler(hello, 2000)
